b95d64419cd118af606adc39ac254e549eddc744
eleunadeu <eleunadeu@gmail.com>
2024-02-21 17:46:06 +0900

    두 수의 차 풀이

---
+++ src/test/TwoNumSub.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TwoNumSub {
+    public static int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(20, 10));
+    }
+}

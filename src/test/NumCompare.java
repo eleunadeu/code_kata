e2c7ecb3b0225d8c055abc646287cadad6acb03c
eleunadeu <eleunadeu@gmail.com>
2024-02-21 19:11:52 +0900

    숫자 비교하기 풀이

---
+++ src/test/NumCompare.java
@@ -0,0 +1,11 @@
+package test;
+
+public class NumCompare {
+    public static int solution(int num1, int num2) {
+        return (num1 == num2) ? 1 : -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3));
+    }
+}

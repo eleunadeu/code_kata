001d7c8fce60cf98a3fc267c8fbdcf6b9d6cba17
eleunadeu <eleunadeu@gmail.com>
2024-02-21 17:50:09 +0900

    두 수의 차 풀이 수정

--- src/test/TwoNumSub.java
+++ src/test/TwoNumSub.java
@@ -2,8 +2,7 @@ package test;
 
 public class TwoNumSub {
     public static int solution(int num1, int num2) {
-        int answer = num1 - num2;
-        return answer;
+        return num1 - num2;
     }
 
     public static void main(String[] args) {

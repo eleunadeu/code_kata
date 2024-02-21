68009db484fb8eecd754ce57fdff91e405c65935
eleunadeu <eleunadeu@gmail.com>
2024-02-21 17:59:16 +0900

    두 수의 몫 풀이

---
+++ src/test/TwoNumDiv.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TwoNumDiv {
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10, 4));
+    }
+}

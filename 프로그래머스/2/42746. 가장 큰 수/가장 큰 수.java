eec5c7f44a827969c5ef2bc4c6ea140592503245
박용운 <121149088+eleunadeu@users.noreply.github.com>
2025-01-03 10:26:24 +0900

    [level 2] Title: 가장 큰 수, Time: 199.36 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        
+        String[] strNumbers = Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .toArray(String[]::new);
+        
+        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+        
+        for(String num : strNumbers) {
+            sb.append(num);
+        }
+        
+        if (sb.toString().charAt(0) == '0') {
+            return "0";
+        }
+        
+        return sb.toString();
+    }
+}
